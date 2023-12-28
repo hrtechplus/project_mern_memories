a787b9f0677fc2b2020230d47b1355128191cbbb
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2023-12-28 15:16:38 +0530

    useStyles

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { AppBar, Typography } from "@material-ui/core";
+import useStyles from "./Style";
 export default function Navbar() {
+  const classes = useStyles();
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <Typography className={classes.heading} variant="h2" align="center">
