5c5ce227effe65e525d1666eb979f1c71a3c78f2
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2023-12-30 12:51:44 +0530

    .

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
 import postRoutes from "./routes/posts.js";
-
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
